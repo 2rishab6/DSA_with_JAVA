9d31888e98934d6e6984648f57ea18280c707213
Rishab Deb Paul <96366218+2rishab6@users.noreply.github.com>
2023-12-28 06:15:16 +0000

    testing file

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ class test {
     public static void main(String[] args)
     {
         int a = 89 ;
-        int b = 456 ; 
+        int b = 45 ;   
         int c = a + b;
         System.out.println(c);
     }
