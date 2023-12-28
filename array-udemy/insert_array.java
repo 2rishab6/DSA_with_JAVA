9163e10b59d1bb27139684934b3e6087d8438554
Rishab Deb Paul <96366218+2rishab6@users.noreply.github.com>
2023-12-28 14:18:23 +0000

    insert array

---
+++ array-udemy/insert_array.java
@@ -0,0 +1,36 @@
+class insert_array{
+
+    int arr[] = null;
+
+    public  insert_array(int array_size)
+    {
+        arr = new int[array_size];
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public void insert(int index, int value)
+    {
+        try{
+            if(arr[index] == Integer.MIN_VALUE){
+                arr[index] = value;
+                System.out.println("Successfully inserted");
+            }else{
+                System.out.println("space is already auocupied");
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("not in array");
+        }
+    }
+
+    public static void main(String[] args){
+        insert_array arr = new insert_array(5);
+        arr.insert(0, 10);
+        arr.insert(1, 20);
+        arr.insert(2, 30);
+        arr.insert(12, 1200);
+        arr.insert(1, 50);
+    }
+}
