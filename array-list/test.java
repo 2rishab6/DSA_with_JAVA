c1afb497b35b88967b46e6dc4202ff6ec117d7fe
Rishab Deb Paul <96366218+2rishab6@users.noreply.github.com>
2024-01-06 04:54:42 +0000

    test

--- array-list/test.java
+++ array-list/test.java
@@ -4,6 +4,8 @@ public class test {
     public static void main(String[] args) {
         
         ArrayList gol = new ArrayList();
+        gol.add("scorpio");
+        gol.add("xuv 700");
         System.out.println(gol);
     }
     
