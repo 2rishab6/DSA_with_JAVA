69d1080f066b72b419ba3fed77aca0a883225ec4
Rishab Deb Paul <96366218+2rishab6@users.noreply.github.com>
2024-01-06 07:29:13 +0000

    test

--- array-list/test.java
+++ array-list/test.java
@@ -3,12 +3,35 @@ public class test {
 
     public static void main(String[] args) {
         
-        ArrayList gol = new ArrayList();
-        gol.add("scorpio");
-        gol.add("xuv 700");
-        gol.add("Thar");
-        gol.add(2,"Lamborgini");
-        System.out.println(gol);
+        ArrayList<String> carlover = new ArrayList<String>();
+        carlover.add("scorpio");
+        carlover.add("xuv 700");
+        carlover.add("Thar");
+        carlover.add(2,"Lamborgini");
+        carlover.add("5");
+        System.out.println(carlover);
+        // System.out.println(gol.get(0));
+        // for (int i = 0 ; i < carlover.size() ; i++){
+        //     System.out.println(carlover.get(i) + " index no = " + i); using for loop
+        // // }
+
+        // foreach loop
+
+        // for ( String letter : carlover){
+        //     System.out.println(letter);
+        // }
+
+        // Search an element
+
+        for ( int i = 0 ; i < carlover.size() ; i++)
+        {
+            if(carlover.get(i) == "Thar"){
+                System.out.println("element found at = " + i + " th position");
+                break;
+            }else
+            {
+                System.out.println("element not found");
+            } 
+        }
     }
-    
 }
